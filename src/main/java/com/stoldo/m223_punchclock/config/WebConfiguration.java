224b8c4b21dde3fb5eb9e77ae9668a86be3d59f3
stoldo <stoldo@runmyaccounts.com>
2020-11-04 13:34:15 +0100

    added base stuff

---
+++ src/main/java/com/stoldo/m223_punchclock/config/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.stoldo.m223_punchclock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .exposedHeaders(
+                        "Access-Control-Allow-Headers",
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Expose-Headers",
+                        "Authorization",
+                        "Cache-Control",
+                        "Content-Type",
+                        "Origin"
+                );
+    }
+}
